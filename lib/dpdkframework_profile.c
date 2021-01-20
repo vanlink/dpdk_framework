85335c2544d7c2386f0e801689cae1a1943542ac
wangxiaofan <paradoxes@163.com>
2021-01-20 16:58:57 +0800

    profile

--- lib/dpdkframework_profile.c
+++ lib/dpdkframework_profile.c
@@ -6,85 +6,9 @@
 
 #include "dkfw_profile.h"
 
-/* cpu统计相关，后续使用 */
-
-static DKFW_PROFILE g_profile_local;
-static DKFW_PROFILE *g_profile = &g_profile_local;
-
-void dkfw_profile_reset(int item_cnt)
+void dkfw_profile_init(DKFW_PROFILE *profile, int item_cnt)
 {
-    memset(g_profile, 0, sizeof(DKFW_PROFILE));
-    g_profile->item_cnt = (item_cnt ? item_cnt : MAX_PROFILE_ITEM_NUM);
-}
-
-void dkfw_profile_reset_extmem(int item_cnt, void *mem)
-{
-    g_profile = mem;
-    dkfw_profile_reset(item_cnt);
-}
-
-void dkfw_profile_start(uint64_t t)
-{
-    g_profile->tmp_start = t;
-}
-
-void dkfw_profile_end(uint64_t t)
-{
-    g_profile->loops_cnt++;
-    g_profile->all_time += (t - g_profile->tmp_start);
-}
-
-void dkfw_profile_item_start(uint64_t t, int i)
-{
-    g_profile->tmp_items_start[i] = t;
-}
-
-void dkfw_profile_item_end(uint64_t t, int i)
-{
-    g_profile->abs_time = t;
-    g_profile->items_time[i] += (t - g_profile->tmp_items_start[i]);
-}
-
-void dkfw_profile_result_get(DKFW_PROFILE *dst)
-{
-    int i;
-        
-    dst->item_cnt = g_profile->item_cnt;
-    dst->loops_cnt = g_profile->loops_cnt;
-    dst->all_time = g_profile->all_time;
-
-    for(i=0;i<g_profile->item_cnt;i++){
-        dst->items_time[i] = g_profile->items_time[i];
-    }
-}
-
-void dkfw_profile_snapshot(DKFW_PROFILE *old)
-{
-    memcpy(old, &g_profile, sizeof(g_profile));
-}
-
-DKFW_PROFILE *dkfw_profile_calc(DKFW_PROFILE *snap)
-{
-    int i;
-    
-    snap->loops_cnt = g_profile->loops_cnt - snap->loops_cnt;
-    snap->abs_time = g_profile->abs_time - snap->abs_time;
-    
-    snap->all_time = g_profile->all_time - snap->all_time;
-    for(i=0;i<MAX_PROFILE_ITEM_NUM;i++){
-        snap->items_time[i] = g_profile->items_time[i] - snap->items_time[i];
-    }
-
-    if(snap->all_time){
-        for(i=0;i<MAX_PROFILE_ITEM_NUM;i++){
-            snap->items_time[i] = snap->items_time[i] * 100 / snap->all_time;
-        }
-    }else{
-        for(i=0;i<MAX_PROFILE_ITEM_NUM;i++){
-            snap->items_time[i] = 100;
-        }
-    }
-
-    return snap;
+    memset(profile, 0, sizeof(DKFW_PROFILE));
+    profile->item_cnt = (item_cnt ? item_cnt : MAX_PROFILE_ITEM_NUM);
 }
 
--- lib/include/dkfw_profile.h
+++ lib/include/dkfw_profile.h
@@ -1,15 +1,13 @@
 #ifndef _DPDK_FRAMEWORK_PROFILE_H
 #define _DPDK_FRAMEWORK_PROFILE_H
+#include <stdint.h>
 
 #define MAX_PROFILE_ITEM_NUM 16
-
-/* cpu 统计相关，后续使用 */
 
 typedef struct _DKFW_PROFILE_TAG {
     int item_cnt;
     
     uint64_t loops_cnt;
-    uint64_t abs_time;
 
     uint64_t all_time;
     uint64_t items_time[MAX_PROFILE_ITEM_NUM];
@@ -18,14 +16,15 @@ typedef struct _DKFW_PROFILE_TAG {
     uint64_t tmp_items_start[MAX_PROFILE_ITEM_NUM];
 } DKFW_PROFILE;
 
-extern void dkfw_profile_reset(int item_cnt);
-extern void dkfw_profile_reset_extmem(int item_cnt, void *mem);
-extern void dkfw_profile_start(uint64_t t);
-extern void dkfw_profile_end(uint64_t t);
-extern void dkfw_profile_item_start(uint64_t t, int i);
-extern void dkfw_profile_item_end(uint64_t t, int i);
-extern void dkfw_profile_snapshot(DKFW_PROFILE *old);
-extern DKFW_PROFILE *dkfw_profile_calc(DKFW_PROFILE *snap);
-extern void dkfw_profile_result_get(DKFW_PROFILE *dst);
+extern void dkfw_profile_init(DKFW_PROFILE *profile, int item_cnt);
+
+#define DKFW_PROFILE_START(profile,t) do { (profile)->tmp_start = t } while(0)
+#define DKFW_PROFILE_END(profile,t) do { \
+    (profile)->loops_cnt++; \
+    (profile)->all_time += ((t) - (profile)->tmp_start); } while(0)
+
+#define DKFW_PROFILE_ITEM_START(profile,t,i) do { profile->tmp_items_start[i] = t; } while(0)
+#define  DKFW_PROFILE_ITEM_END(profile,t,i) do { profile->items_time[i] += ((t) - profile->tmp_items_start[i]); } while(0)
+
 #endif
 
