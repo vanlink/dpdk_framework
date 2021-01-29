a0af53b570502fca78a9222ad16976925b57537c
wangxiaofan <paradoxes@163.com>
2021-01-29 22:07:51 +0800

    cps

--- lib/dpdkframework_cps.c
+++ lib/dpdkframework_cps.c
@@ -15,16 +15,51 @@ void dkfw_cps_create(DKFW_CPS *dkfwcps, uint64_t cps, uint64_t tsc_per_sec)
     dkfwcps->tsc_per_sec = tsc_per_sec;
 }
 
-uint64_t dkfw_cps_get(DKFW_CPS *dkfwcps, uint64_t tsc)
+static inline uint64_t get_curr_cps(DKFW_CPS *dkfwcps, uint64_t ms)
+{
+    DKFW_CPS_SEG *seg = &dkfwcps->cps_segs[dkfwcps->cps_seg_curr_ind];
+    uint64_t offset_ms;
+
+    if(unlikely(!seg->seg_total_ms)){
+        return seg->cps_start;
+    }
+
+    if(unlikely(!seg->seg_start_ms)){
+        seg->seg_start_ms = ms;
+        return seg->cps_start;
+    }
+
+    offset_ms = ms - seg->seg_start_ms;
+
+    if(unlikely(offset_ms >= seg->seg_total_ms)){
+        dkfwcps->cps_seg_curr_ind++;
+        return seg->cps_end;
+    }
+
+    if(seg->cps_start == seg->cps_end){
+        return seg->cps_start;
+    }
+
+    if(seg->cps_start < seg->cps_end){
+        return seg->cps_start + offset_ms * (seg->cps_end - seg->cps_start) / seg->seg_total_ms;
+    }else{
+        return seg->cps_start - offset_ms * (seg->cps_start - seg->cps_end) / seg->seg_total_ms;
+    }
+}
+
+uint64_t dkfw_cps_get(DKFW_CPS *dkfwcps, uint64_t tsc, uint64_t ms)
 {
     uint64_t diff, cnt;
+    uint64_t cps;
 
     if(unlikely(!dkfwcps->tsc_last)){
         dkfwcps->tsc_last = tsc;
         return 0;
     }
 
-    diff = (tsc - dkfwcps->tsc_last) * dkfwcps->cps;
+    cps = get_curr_cps(dkfwcps, ms);
+
+    diff = (tsc - dkfwcps->tsc_last) * cps;
 
     if(likely(diff < dkfwcps->tsc_per_sec)){
         return 0;
@@ -32,14 +67,14 @@ uint64_t dkfw_cps_get(DKFW_CPS *dkfwcps, uint64_t tsc)
 
     cnt = diff / dkfwcps->tsc_per_sec;
 
-    dkfwcps->tsc_last += dkfwcps->tsc_per_sec * cnt / dkfwcps->cps;
+    dkfwcps->tsc_last += dkfwcps->tsc_per_sec * cnt / cps;
 
     return cnt;
 }
 
-uint64_t dkfw_cps_limited_get(DKFW_CPS *dkfwcps, uint64_t tsc)
+uint64_t dkfw_cps_limited_get(DKFW_CPS *dkfwcps, uint64_t tsc, uint64_t ms)
 {
-    uint64_t cnt = dkfw_cps_get(dkfwcps, tsc);
+    uint64_t cnt = dkfw_cps_get(dkfwcps, tsc, ms);
 
     if(likely(cnt == 0)){
         if(unlikely(dkfwcps->send_cnt_remain)){
--- lib/include/dkfw_cps.h
+++ lib/include/dkfw_cps.h
@@ -2,6 +2,17 @@
 #define _DPDK_FRAMEWORK_CPS_H
 #include <stdint.h>
 
+#define DKFW_CPS_SEGS_MAX 8
+
+typedef struct DKFW_CPS_SEG_TAG {
+    uint64_t cps_start;
+    uint64_t cps_end;
+
+    uint64_t seg_total_ms;
+
+    uint64_t seg_start_ms;
+} DKFW_CPS_SEG;
+
 typedef struct DKFW_CPS_TAG {
     uint64_t cps;
     uint64_t tsc_last;
@@ -9,11 +20,13 @@ typedef struct DKFW_CPS_TAG {
 
     uint64_t send_cnt_remain;
 
+    int cps_seg_curr_ind;
+    DKFW_CPS_SEG cps_segs[DKFW_CPS_SEGS_MAX];
 } DKFW_CPS;
 
 extern void dkfw_cps_create(DKFW_CPS *dkfwcps, uint64_t cps, uint64_t tsc_per_sec);
-extern uint64_t dkfw_cps_get(DKFW_CPS *dkfwcps, uint64_t tsc);
-extern uint64_t dkfw_cps_limited_get(DKFW_CPS *dkfwcps, uint64_t tsc);
+extern uint64_t dkfw_cps_get(DKFW_CPS *dkfwcps, uint64_t tsc, uint64_t ms);
+extern uint64_t dkfw_cps_limited_get(DKFW_CPS *dkfwcps, uint64_t tsc, uint64_t ms);
 
 #endif
 
