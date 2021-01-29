6c32592a2daca6bc1708f49a9b99907e87c3ea4c
wangxiaofan <paradoxes@163.com>
2021-01-29 17:03:53 +0800

    profile

--- lib/dpdkframework_profile.c
+++ lib/dpdkframework_profile.c
@@ -6,10 +6,11 @@
 
 #include "dkfw_profile.h"
 
-void dkfw_profile_init(DKFW_PROFILE *profile, int item_cnt)
+void dkfw_profile_init(DKFW_PROFILE *profile, int item_cnt, int single_cnt)
 {
     memset(profile, 0, sizeof(DKFW_PROFILE));
     profile->item_cnt = (item_cnt ? item_cnt : MAX_PROFILE_ITEM_NUM);
+    profile->single_cnt = single_cnt;
 }
 
 cJSON *dkfw_profile_to_json(DKFW_PROFILE *profile)
@@ -17,7 +18,7 @@ cJSON *dkfw_profile_to_json(DKFW_PROFILE *profile)
     int i;
     char buff[64];
     cJSON *root = cJSON_CreateObject();
-    cJSON *json_array = cJSON_CreateArray();
+    cJSON *json_array;
 
     cJSON_AddItemToObject(root, "item_cnt", cJSON_CreateNumber(profile->item_cnt));
 
@@ -27,13 +28,20 @@ cJSON *dkfw_profile_to_json(DKFW_PROFILE *profile)
     sprintf(buff, "%lu", profile->all_time);
     cJSON_AddItemToObject(root, "all_time", cJSON_CreateString(buff));
 
+    json_array = cJSON_CreateArray();
     for(i=0;i<profile->item_cnt;i++){
         sprintf(buff, "%lu", profile->items_time[i]);
         cJSON_AddItemToArray(json_array, cJSON_CreateString(buff));
     }
     cJSON_AddItemToObject(root, "items_time", json_array);
 
+    json_array = cJSON_CreateArray();
+    for(i=0;i<profile->single_cnt;i++){
+        sprintf(buff, "%lu", profile->single_time[i]);
+        cJSON_AddItemToArray(json_array, cJSON_CreateString(buff));
+    }
+    cJSON_AddItemToObject(root, "singles_time", json_array);
+
     return root;
 }
-
 
--- lib/include/dkfw_profile.h
+++ lib/include/dkfw_profile.h
@@ -6,18 +6,23 @@
 #define MAX_PROFILE_ITEM_NUM 16
 
 typedef struct _DKFW_PROFILE_TAG {
-    int item_cnt;
 
     uint64_t loops_cnt;
 
     uint64_t all_time;
+
+    int item_cnt;
     uint64_t items_time[MAX_PROFILE_ITEM_NUM];
+
+    int single_cnt;
+    uint64_t single_time[MAX_PROFILE_ITEM_NUM];
 
     uint64_t tmp_start;
     uint64_t tmp_items_start[MAX_PROFILE_ITEM_NUM];
+    uint64_t tmp_single_start[MAX_PROFILE_ITEM_NUM];
 } DKFW_PROFILE;
 
-extern void dkfw_profile_init(DKFW_PROFILE *profile, int item_cnt);
+extern void dkfw_profile_init(DKFW_PROFILE *profile, int item_cnt, int single_cnt);
 extern cJSON *dkfw_profile_to_json(DKFW_PROFILE *profile);
 
 #define DKFW_PROFILE_START(profile,t) do { (profile)->tmp_start = t; } while(0)
@@ -27,6 +32,9 @@ extern cJSON *dkfw_profile_to_json(DKFW_PROFILE *profile);
 
 #define DKFW_PROFILE_ITEM_START(profile,t,i) do { profile->tmp_items_start[i] = t; } while(0)
 #define  DKFW_PROFILE_ITEM_END(profile,t,i) do { profile->items_time[i] += ((t) - profile->tmp_items_start[i]); } while(0)
+
+#define DKFW_PROFILE_SINGLE_START(profile,t,i) do { profile->tmp_single_start[i] = t; } while(0)
+#define  DKFW_PROFILE_SINGLE_END(profile,t,i) do { profile->single_time[i] += ((t) - profile->tmp_single_start[i]); } while(0)
 
 #endif
 
